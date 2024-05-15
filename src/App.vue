730b8f824c2b81185b5bc73b36524753f5600ed7
ilhammdd <165543551+ilhammmdd@users.noreply.github.com>
2024-05-15 16:10:42 +0700

    per 10

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
+<script setup>
+import { ref, watchEffect } from 'vue';
+
+const users = ref([]);
+const posts = ref([]);
+const selectedUser = ref(null);
+
+async function fetchUsers() {
+  const url = 'https://jsonplaceholder.typicode.com/users';
+  const response = await fetch(url);
+  users.value = await response.json();
+}
+
+async function fetchPosts(userId) {
+  const url = `https://jsonplaceholder.typicode.com/posts?userId=${userId}`;
+  const response = await fetch(url);
+  const fetchedPosts = await response.json();
+  posts.value = fetchedPosts.sort((a, b) => a.id - b.id);
+}
+
+fetchUsers();
+
+watchEffect(() => {
+  if (selectedUser.value) {
+    fetchPosts(selectedUser.value);
+  }
+});
+</script>
+
+
+
 <template>
 <div class="wrapper">
-  <nav class="navbar" :class="{ 'slide-down': showNavbar }">
-  <a class="navbar-item" href="#" @click.prevent="showNavbar = !showNavbar">Todo</a>
-  <a class="navbar-item" href="#" @click.prevent="showNavbar = !showNavbar">Post</a>
+  <nav class="navbar">
+  <button @click="selectedMenu = 'todos'">TODOS</button>
+  <button @click="selectedMenu = 'post'">POST</button>
 </nav>
+
+<div v-show="selectedMenu == 'todos'">
   <div class="container" :class="{ 'fadeIn': fadeIn, 'fadeOut': fadeOut }">
     <h1>Plan Your Activity</h1>
     <table>
@@ -43,12 +76,35 @@
       Show Unfinished Tasks
     </label>
   </div>
+  </div>
+
+
+  <div v-show="selectedMenu == 'post'">
+      <h1 class="tulis">User</h1>
+      <div style="display: flex; justify-content: center;">
+      <select v-model="selectedUser" style="text-align: center;">
+        <option v-for="user in users" :key="user.id" :value="user.id" style="text-align: center;">{{ user.name }}</option>
+      </select>
+    </div>
+
+      <h1 class="tulis">Select User : {{ selectedUser }}</h1><br>
+
+      <h1 class="tulis">Post</h1>
+      <ol>
+      <li v-for="(post, index) in posts" :key="post.id" class="tulis">
+        <span>{{ }}</span> {{ users.find((user) => user.id === post.userId).name }} -
+        <b>{{ post.title }}</b> - {{ post.body }}
+      </li>
+      </ol>
+    </div>
 </div>
+
 
   <footer class="footer" @mouseover="footerHover">
     ILHAM | 2024 | Universitas Islam Riau
   </footer>
 </template>
+
 
 <script>
 export default {
@@ -58,7 +114,8 @@ export default {
       newActivity: '',
       showOnlyIncomplete: false,
       fadeIn: false,
-      fadeOut: false
+      fadeOut: false,
+      selectedMenu: 'todos'
     };
   },
   created() {
@@ -93,8 +150,6 @@ export default {
           completed: false
         });
         this.newActivity = '';
-        
-       
         this.fadeIn = true;
         setTimeout(() => {
           this.fadeIn = false;
